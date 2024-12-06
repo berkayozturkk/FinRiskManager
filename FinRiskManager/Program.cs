ac351ba8d80157247b4c820664929c8654f42d8a
Berkay Öztürk <73599896+berkayozturkk@users.noreply.github.com>
2024-12-06 23:27:27 +0300

    FinRiskManager veritabanı bağlantısının kurulması

--- FinRiskManager/Program.cs
+++ FinRiskManager/Program.cs
@@ -1,7 +1,13 @@
+using FinanceRiskManagement.Data.Contexts;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<BaseDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("sqlConnection")));
 
 var app = builder.Build();
 
