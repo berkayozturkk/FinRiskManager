fc06e245d3fdc4925c18968d275d64ee300f0515
Berkay Öztürk <73599896+berkayozturkk@users.noreply.github.com>
2024-12-14 12:41:10 +0300

    DahsboardController oluşturulması

---
+++ FinRiskManager/Controllers/DashboardController.cs
@@ -0,0 +1,28 @@
+﻿using FinRiskManager.Business.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FinRiskManager.Controllers
+{
+    public class DashboardController : Controller
+    {
+        private readonly IAgreementService _agreementService;
+
+        public DashboardController(IAgreementService agreementService)
+        {
+            _agreementService = agreementService;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // Anlaşmaları JSON formatında döner
+        [HttpGet("GetAgreements")]
+        public async Task<IActionResult> GetAgreements()
+        {
+            var agreements = await _agreementService.GetAllAgreementsAsync();
+            return Ok(agreements);
+        }
+    }
+}
